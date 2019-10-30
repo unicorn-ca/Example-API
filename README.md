56a8b644ebabf1e2961ca0ebdb873485ddeed01d
uint0 <czhou428420713@gmail.com>
2019-10-30 20:05:35 +1100

    Start testing

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ We leverage CodeBuild and serverless (sls) in order to build and deploy a simple
 API.
 
 The Code is cloned to CodeCommit where the Unicorn Pipeline begins deploying it.
+
+## Editing
+There are two build files `serverless.yml` and `buildspec.build.yaml`.
+
+Buildspec is used to define the build process for AWS CodeBuild. This currently
+includes some hacks to format the serverless output into a fashion that fits
+into the deployed pipeline. We aim to fix this by the next iteration.
+
+Serverless is a typical serverless configuration file. Not that a `deploymentBucket` should **not** be defined as this will cause a build to fail.
--- handler.py
+++ handler.py
@@ -1,3 +1,4 @@
+# Deploy magic: 1
 import json, os
 import psycopg2
 
@@ -78,41 +79,3 @@ def sqli_secure(event, context):
     }
 
     return response
-
-"""
-def xss_vulnerable(event, context):
-    body = {}
-
-    if event["queryStringParameters"] is not None:
-        vulnString = event["queryStringParameters"].get("vuln-string")
-        
-        body["message"] = "Success"
-        body["vulnString"] = vulnString
-        
-        query = "SELECT * FROM test WHERE username=%s" % vulnString
-
-        # create a cursor
-        cur = conn.cursor()
-
-        # execute a statement
-        cur.execute("SELECT * FROM test WHERE username=%s", (vulnString, ))
-        
-        # display the result
-        result = cur.fetchone()
-        
-        # close the communication with the PostgreSQL
-        cur.close()
-        
-        body["query"] = query
-        body["result"] = result
-    else:
-        body["message"] = "Please inject some SQL with ?vuln-string="
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-"""
